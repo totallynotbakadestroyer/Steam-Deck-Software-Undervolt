e6a75192eef3ea94911f4776527f431d6bcbec39
Aleksey Tarasov <al.tarasov@pjpizza.ru>
2023-10-12 00:22:07 +0400

    Add uninstall curve optimizer

--- uninstall.sh
+++ uninstall.sh
@@ -15,20 +15,30 @@ systemctl disable set-ryzenadj-tweaks.service
 echo "Disable path listener..."
 systemctl disable set-ryzenadj-tweaks.path
 
+echo "Disable set-ryzenadj-curve service..."
+systemctl disable set-ryzenadj-curve.service
+
+echo "Disable path listener..."
+systemctl disable set-ryzenadj-curve.path
+
 echo "Removing files..."
 remPath="/home/deck/.local/bin/"
 rm $remPath"allowadj.txt"
+rm $remPath"curve.sh"
 rm $remPath"experimental.sh"
 rm $remPath"experimentaladj.txt"
 rm $remPath"off.sh"
 rm $remPath"on.sh"
 rm $remPath"ryzenadj"
+rm $remPath"set-ryzenadj-curve.sh"
 rm $remPath"set-ryzenadj-tweaks.sh"
 rm $remPath"statusadj.txt"
 
 remPath="/etc/systemd/system/"
 rm $remPath"ac.target"
 rm $remPath"battery.target"
+rm $remPath"set-ryzenadj-curve.path"
+rm $remPath"set-ryzenadj-curve.service"
 rm $remPath"set-ryzenadj-tweaks.path"
 rm $remPath"set-ryzenadj-tweaks.service"
 
